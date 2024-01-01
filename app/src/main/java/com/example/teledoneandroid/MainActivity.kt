cbc905e5e2a15b6013ddc7a1c67c70aa9f3ed141
Александр Гладун <alexandr20021995@gmail.com>
2024-01-01 21:33:54 +0600

    Add new task UI

--- app/src/main/java/com/example/teledoneandroid/MainActivity.kt
+++ app/src/main/java/com/example/teledoneandroid/MainActivity.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,7 +22,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,7 +50,8 @@ fun ComposeTeledoneApp() {
     Column {
         TopPanel(modifier = Modifier.weight(0.1f))
         MainPanel(modifier = Modifier.weight(0.1f))
-        InfoPanel(modifier = Modifier.weight(0.7f))
+        InfoPanel(modifier = Modifier.weight(0.6f))
+        NewTaskPanel(modifier = Modifier.weight(0.1f).align(Alignment.End))
         CategoryPanel(modifier = Modifier.weight(0.1f))
     }
 }
@@ -142,6 +149,27 @@ fun InfoPanel(modifier: Modifier = Modifier) {
         Task()
         Task()
         Task()
+    }
+}
+
+@Composable
+fun NewTaskPanel(modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+            modifier = Modifier
+                .fillMaxHeight(),
+            content = {
+                Image(
+                    painter = painterResource(id = R.drawable.plus__96),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                )
+            }
+        )
     }
 }
 
